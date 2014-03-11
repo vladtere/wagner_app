136582197195ae92c868a28f888f602ccf5fa52f
vlad <vlad_tere@yahoo.com>
2014-03-11 16:13:05 +0100

    Primu comment la wagnerapp

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WagnerApp::Application.config.secret_key_base = '922eaf7ffdbb00b360c6fc3f6d6714cd525effe536d89d1cd6a71c3bf8025418a1fd69153e94b6795e037620a65e270a613e49681b1541337cd46b74a4e08d7a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WagnerApp::Application.config.session_store :cookie_store, key: '_wagner_app_session'
